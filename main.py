9cd95aa6768dd36a717473241a84762afd95244e
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2025-02-10 19:41:13 -0300

    insert

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
-from config import ALPHA, CARACTER, arr
+from config import ALPHA, CARACTER, NUMBER, arr
 import random
 import os
 import time
 
 def clean_terminal():
-    time.sleep(3)
+    time.sleep(2)
     os.system("cls")
 
-def size(arr, n):
-    count = 0
+def insert_all(n):
+    c = 0
+    while c < n:
+        c += 1
+        for i in range( n // 2):
+            c += 1
+            l = random.choice(ALPHA)
+            arr.append(l)
+       
+        nu = random.choice(NUMBER)
+        arr.append(nu)
 
-    while count < n:
-        arr.append(" ") 
-        count += 1
+        ca = random.choice(CARACTER)
+        arr.append(ca)
     return arr
 
+def insert_letter(n):
+    arr.extend(random.choice(ALPHA) for _ in range(n))
+    return arr
+ 
+def insert_number(n):
+    arr.extend(random.choice(NUMBER)for _ in range(n))
+    return arr
+
+def insert_especial(n):
+    arr.extend(random.choice(CARACTER)for _ in range(n))
+    return arr
+
+def string(c):
+
+    n = c
+    lista = insert_all(n)
+    new_list = "".join([str(x) for x in lista])
+    return new_list
